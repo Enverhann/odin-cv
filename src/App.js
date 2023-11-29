55cea08e0dabaa2bac9a975b4a29c18365b9a893
Enverhann <enverhanoksuz@hotmail.com>
2023-11-29 14:38:38 +0300

    footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import General from './General';
 import Education from './Education';
 import Experience from './Experience';
+import Footer from './Footer';
 
 function App() {
 const handleSubmit = (section,data) => {
@@ -14,6 +15,7 @@ const handleSubmit = (section,data) => {
       <General onSubmit={(data) => handleSubmit('General Info', data)} />
       <Education onSubmit={(data) => handleSubmit('Educational Experience', data)} />
       <Experience onSubmit={(data) => handleSubmit('Practical Experience', data)} />
+      <Footer/>
     </div>
   );
 }
